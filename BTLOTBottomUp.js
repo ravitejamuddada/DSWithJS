08dfbe69681984aa6d4cbc7174f75c46db6fc86b
Ravi Teja Muddada <33419617+myjscoffee@users.noreply.github.com>
2020-08-02 22:02:04 +0530

    Create BTLOTBottomUp.js

---
+++ BTLOTBottomUp.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom =  function(root) {
+    let res=[];
+    let level=0;
+    const LT=(root,level)=>{
+        if(!root) return;
+        if(res[level]){
+            res[level].push(root.val)
+        }
+        else{
+            res[level]=[root.val]
+        }
+        LT(root.left,level+1)
+        LT(root.right,level+1)
+    }
+    LT(root,0);
+    return res.reverse();
+};
