d31cc106327651da79bb463a2f0682fe4bab71cb
Ravi Teja Muddada <33419617+myjscoffee@users.noreply.github.com>
2020-08-18 22:27:47 +0530

    Create levelOrderTraversal.js

---
+++ BT/levelOrderTraversal.js
@@ -0,0 +1,20 @@
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    let res=[];
+    const lot=(root,l)=>{
+        if(!root) return
+        if(res[l]){
+            res[l].push(root.val);
+        }
+        else{
+            res[l]=[root.val];
+        }
+        lot(root.left,l+1);
+        lot(root.right,l+1);
+    }
+    lot(root,0);
+    return res;
+};
